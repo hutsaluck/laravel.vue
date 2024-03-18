6e1f468de1a562c68ea9b44ac113eab15cdb5b5e
Юра Гуцалюк <hutsaluck@gmail.com>
2024-03-18 16:30:00 +0200

    Add Category of Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Category::factory(5)->create();
         \App\Models\Post::factory(50)->create();
 
         // \App\Models\User::factory()->create([
