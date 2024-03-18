17139c91c6c1f42c4016f573347742c240872217
Юра Гуцалюк <hutsaluck@gmail.com>
2024-03-18 16:17:38 +0200

    Add Pagination of Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\Post::factory(10)->create();
+        \App\Models\Post::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
