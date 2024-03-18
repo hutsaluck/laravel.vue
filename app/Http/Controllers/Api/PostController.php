0ffa46cb2f2c97fb24350a5a1fe5831acfe292ba
Юра Гуцалюк <hutsaluck@gmail.com>
2024-03-18 19:56:46 +0200

    Add create functional of Post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -12,22 +13,29 @@ class PostController extends Controller
 {
     public function index()
     {
-        $orderColumn = request('order_column', 'created_at');
-        if (! in_array($orderColumn, ['id', 'title', 'created_at'])) {
+        $orderColumn = request( 'order_column', 'created_at' );
+        if ( !in_array( $orderColumn, [ 'id', 'title', 'created_at' ] ) ) {
             $orderColumn = 'created_at';
         }
-        $orderDirection = request('order_direction', 'desc');
-        if (! in_array($orderDirection, ['asc', 'desc'])) {
+        $orderDirection = request( 'order_direction', 'desc' );
+        if ( !in_array( $orderDirection, [ 'asc', 'desc' ] ) ) {
             $orderDirection = 'desc';
         }
 
         $posts = Post::with( 'category' )
-            ->when(request('category'), function (Builder $query) {
-                $query->where('category_id', request('category'));
-            })
-            ->orderBy($orderColumn, $orderDirection)
+            ->when( request( 'category' ), function ( Builder $query ) {
+                $query->where( 'category_id', request( 'category' ) );
+            } )
+            ->orderBy( $orderColumn, $orderDirection )
             ->paginate( 10 );
 
         return PostResource::collection( $posts );
+    }
+
+    public function store( StorePostRequest $request )
+    {
+        $post = Post::create( $request->validated() );
+
+        return new PostResource( $post );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware( 'auth:sanctum' )->get( '/user', function ( Request $request ) {
-    return $request->user();
-} );
-
-Route::get( 'posts', [ PostController::class, 'index' ] );
+Route::apiResource('posts', PostController::class);
 Route::get('categories', [CategoryController::class, 'index']);
