10cb731bb1ef830bd6c2ac7538e2a351ae82f962
Юра Гуцалюк <hutsaluck@gmail.com>
2024-03-25 10:10:57 +0200

    Disable delete of Editor

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,14 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
         return $request->user();
     });
 });
+
+Route::get('abilities', function(Request $request) {
+    return $request->user()->roles()->with('permissions')
+        ->get()
+        ->pluck('permissions')
+        ->flatten()
+        ->pluck('name')
+        ->unique()
+        ->values()
+        ->toArray();
+});
